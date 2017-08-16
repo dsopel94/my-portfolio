9f9736975ed8f5532d526e2323d4a5e14d753949
Damian Sopel <dsopel0@gmail.com>
2017-08-16 18:19:18 -0500

    new shiny animations

--- js.js
+++ js.js
@@ -10,15 +10,41 @@ $('.menu').click('a', function(event) {
 		$(".menu").slideToggle("slow");
 	});
 
+
+
+
 $(window).scroll(function() {
   
+	var projects =$('#greeting').offset(); 
+	var about_me =$('#pre-about-me').offset();
+	var skills = $('#skills-intro').offset();
+	var contact= $('#contact-intro').offset();
+
   // selectors
   var $window = $(window),
       $body = $('body'),
-      $section = $('.section');
+      $section = $('.section'),
+      $nav = $('nav a')
   
   // Change 33% earlier than scroll position so colour is there when you arrive.
   var scroll = $window.scrollTop() + ($window.height() / 3);
+
+//   if( $(window).scrollTop()+$('.nav-bar').height()>=about_me.top+10 &&
+//    $(window).scrollTop()+$('.nav-bar').height()< skills.top+10 )
+// {
+//     $('#skills-nav').css('color','blue');
+//     console.log("scrolled down here")
+// }
+
+// 	else if ($(window).scrollTop() < skills.top+10)
+// 	{
+//     	$('#skills-nav').css('color','white');
+// 	}
+
+// 	if ($(window).scrollTop() >= skills.top)
+// 	{
+// 		$('#contact-nav'.css('color', 'green'));
+// 	}
  
   $section.each(function () {
     var $this = $(this);
@@ -32,9 +58,15 @@ $(window).scroll(function() {
       $body.removeClass(function (index, css) {
         return (css.match (/(^|\s)color-\S+/g) || []).join(' ');
       });
+
+      $nav.removeClass(function (index, css) {
+        return (css.match (/(^|\s)color-\S+/g) || []).join(' ');
+      });
+
        
       // Add class of currently active div
       $body.addClass('color-' + $(this).data('color'));
+      $nav.addClass('color-' + $(this).data('color'));
     }
   });    
   
